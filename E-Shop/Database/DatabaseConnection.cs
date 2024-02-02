1ea5944fc26901aa59635e93c7832979111e0b16
flick <flick@FAHAD>
2024-02-02 22:15:01 +0600

    Add project files.

---
+++ E-Shop/Database/DatabaseConnection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace E_Shop.Database
+{
+    public abstract class DatabaseConnection
+    {
+        const string connectionString = @"Data Source=Fahad\SQLEXPRESS;Initial Catalog=E-Shop;Integrated Security=True";
+        public static SqlConnection databaseConnect()
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
